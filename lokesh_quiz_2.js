1546b51920d41e7c8becfe3e398c4c9651e1ab3f
TechnoWebSeries <71449488+TechnoWebSeries@users.noreply.github.com>
2020-09-19 23:03:58 +0530

    Add files via upload

---
+++ lokesh_quiz_2.js
@@ -0,0 +1,134 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "If one-third of one-fourth of a number is 15,then three-tenth of that number is:",
+    options: ["35", "36", "45", "54"],
+    answer: 3
+  }, {
+    question: "Three times the first of three consecutive odd integers is 3 more than twice the third.The third integer is:",
+    options: ["9", "11", "13", "15"],
+    answer: 3
+  }, {
+    question: "The difference between a two digit number and a number obtained by interchanging the positions of its digits is 36.what is the difference between the two digits of that number:",
+    options: ["3", "4", "9","none of the above"],
+    answer: 1
+  },{
+    question: "The difference between a two digit number and a number obtained by interchanging the positions of its digits is 36.what is the difference between the sum and difference of the digits of the number if the ratio between the digits of the number is 1:2?",
+    options: ["4", "8", "16", "none of the above"],
+    answer: 1
+  }, {
+    question: "A two digit number is such that the product of the digits is 8.when 18 is added to the number,then the digits are reversed.the number is:",
+    options: ["18", "24", "81", "42"],
+    answer: 1
+  }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+            alert('Please select an option !');
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
+    {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
